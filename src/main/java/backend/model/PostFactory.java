6e088de73f9e02a777eef58d6c8c82c78fff959f
Vojtěch Bartička <aitakaitov@gmail.com>
2022-01-03 19:12:12 +0100

    fixed issue with time in database and jpa

--- src/main/java/backend/model/PostFactory.java
+++ src/main/java/backend/model/PostFactory.java
@@ -5,6 +5,7 @@ import backend.model.entity.TypeEntity;
 import backend.model.entity.UserEntity;
 
 import java.sql.Time;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
@@ -13,7 +14,7 @@ public class PostFactory {
         PostEntity e = new PostEntity();
         e.setText(text);
         e.setUser_email(userEmail);
-        e.setTime_posted(Time.valueOf(LocalTime.now()));
+        e.setTime_posted(Timestamp.valueOf(LocalDateTime.now()));
 
         return e;
     }
